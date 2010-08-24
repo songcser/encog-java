1c28b51848403b2ac010ddecbbc7fa28345e86b0
Seemasingh8614 <Seemasingh8614@yahoo.com>
2010-08-24 21:11:37 +0000

    Added build file and unit tests to Javascript plugin.

--- encogscript-javascript/src/org/encog/script/javascript/EncogJavascriptEngineFactory.java
+++ encogscript-javascript/src/org/encog/script/javascript/EncogJavascriptEngineFactory.java
@@ -1,5 +1,24 @@
 package org.encog.script.javascript;
 
-public class EncogJavascriptEngineFactory {
+import org.encog.persist.EncogMemoryCollection;
+import org.encog.script.EncogScriptEngine;
+import org.encog.script.IndividualEngineFactory;
+
+public class EncogJavascriptEngineFactory implements IndividualEngineFactory {
+
+	@Override
+	public EncogScriptEngine create() {
+		return new EncogJavascriptEngine();
+	}
+
+	@Override
+	public EncogScriptEngine create(EncogMemoryCollection c) {
+		return new EncogJavascriptEngine(c);
+	}
+
+	@Override
+	public String getName() {
+		return "javascript";
+	}
 
 }
--- encogscript-javascript/test/org/encog/script/javascript/TestJavaScript.java
+++ encogscript-javascript/test/org/encog/script/javascript/TestJavaScript.java
@@ -2,16 +2,29 @@ package org.encog.script.javascript;
 
 
 import org.encog.script.EncogScript;
-import org.encog.script.StandardConsole;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class TestJavaScript extends TestCase {
 
 	public void testHelloWorld()
 	{
+		StringConsole console = new StringConsole();
+		EncogJavascriptEngine.init();
 		EncogScript script = new EncogScript();
-		script.setSource("print(\'Hello World\')\n");
-		script.run(new StandardConsole());
+		script.setSource("console.print(\'Hello World\')\n");
+		script.run(console);
+		Assert.assertEquals(console.toString(),"Hello World");		
+	}
+	
+	public void testLoop()
+	{
+		StringConsole console = new StringConsole();
+		EncogJavascriptEngine.init();
+		EncogScript script = new EncogScript();
+		script.setSource("for(i=0;i<10;i++){console.print(i);}\n");
+		script.run(console);
+		Assert.assertEquals(console.toString(),"0.01.02.03.04.05.06.07.08.09.0");		
 	}
 }
