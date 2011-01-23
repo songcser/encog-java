e61cb5e6f8237bf1bab9459f6c3a72a24d71e0da
jeffheatondotcom <jeffheatondotcom@f90f6e9a-ac51-0410-b353-d1b83c6f6923>
2011-01-23 03:17:57 +0000

    Encog workbench 2.5

--- encog-workbench/src/org/encog/workbench/EncogWorkBench.java
+++ encog-workbench/src/org/encog/workbench/EncogWorkBench.java
@@ -65,7 +65,7 @@ public class EncogWorkBench implements Runnable {
 
 	public final static String CONFIG_FILENAME = ".EncogWorkbench.conf";
 	public final static String VERSION = "2.5.3";
-	public static final String COPYRIGHT = "Copyright 2010 by Heaton Research, Inc.";
+	public static final String COPYRIGHT = "Copyright 2011 by Heaton Research, Inc.";
 
 	/**
 	 * The singleton instance.
