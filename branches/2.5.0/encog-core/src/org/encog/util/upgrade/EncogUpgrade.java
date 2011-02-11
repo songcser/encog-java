aaa59dc6246abac3469d825427e597f0c699b3d5
Seemasingh8614@yahoo.com <Seemasingh8614@yahoo.com@f90f6e9a-ac51-0410-b353-d1b83c6f6923>
2011-02-11 21:02:00 +0000

    2.5: Added EncogUpgrade to save neural networks to 3.0 format.

---
+++ branches/2.5.0/encog-core/src/org/encog/util/upgrade/EncogUpgrade.java
@@ -0,0 +1,10 @@
+package org.encog.util.upgrade;
+
+import org.encog.neural.networks.BasicNetwork;
+
+public class EncogUpgrade {
+	public void performUpgrade(String filename, BasicNetwork network)
+	{
+		
+	}
+}
